9373d5c992089b3421bfe6788323f5523191c452
gonghsunchen-a11y <gonghsunchen@gmail.com>
2026-05-23 18:34:41 +0800

    Update BLE UUIDs and add authentication mechanism

--- BLE_Codes/master0502.ino
+++ BLE_Codes/master0502.ino
@@ -2,8 +2,10 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 
-#define SERVICE_UUID               "458063a1-02bf-4664-857e-16c1030be066"
-#define DATA_CHARACTERISTIC_UUID   "a5209632-66a9-411d-9353-9be5507790fa"
+#define SERVICE_UUID               "08001234-1234-4321-4321-123456789012"
+#define DATA_CHARACTERISTIC_UUID   "66666666-6666-6666-6666-666666666666"
+#define SECRET_KEY                 0x66
+#define AUTH_PASSWORD              0x66
 
 typedef struct {
   uint8_t ball_dist;
@@ -15,6 +17,7 @@ typedef struct {
 RobotData slaveData;
 bool newData = false;
 bool slaveConnected = false;
+bool authenticated = false;
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pDataCharacteristic = NULL;
@@ -41,11 +44,12 @@ void rgbLEDWrite(uint8_t r, uint8_t g, uint8_t b) {
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
     slaveConnected = true;
-    rgbLEDWrite(0, 255, 0);
-    Serial.println("Slave connected");
+    authenticated = false;
+    Serial.println("裝置連上，等待驗證...");
   }
   void onDisconnect(BLEServer *pServer) {
     slaveConnected = false;
+    authenticated = false;
     rgbLEDWrite(255, 0, 0);
     Serial.println("Slave disconnected");
     pServer->getAdvertising()->start();
@@ -55,8 +59,29 @@ class MyServerCallbacks : public BLEServerCallbacks {
 class MyDataCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
     String value = pCharacteristic->getValue();
-    if (value.length() == sizeof(RobotData)) {
-      memcpy(&slaveData, value.c_str(), sizeof(RobotData));
+
+    if (!authenticated) {
+      if (value.length() == 4) {
+        uint32_t pwd;
+        memcpy(&pwd, value.c_str(), 4);
+        if (pwd == AUTH_PASSWORD) {
+          authenticated = true;
+          rgbLEDWrite(0, 255, 0);
+          Serial.println("驗證成功");
+        } else {
+          Serial.println("密碼錯誤，斷線");
+          rgbLEDWrite(255, 0, 0);
+          pServer->disconnect(0);
+        }
+      }
+      return;
+    }
+
+    if (value.length() == 4) {
+      slaveData.ball_dist  = value[0] ^ SECRET_KEY;
+      slaveData.ball_angle = value[1] ^ SECRET_KEY;
+      slaveData.robot_x    = value[2] ^ SECRET_KEY;
+      slaveData.robot_y    = value[3] ^ SECRET_KEY;
       newData = true;
     }
   }
@@ -67,7 +92,8 @@ void setup() {
   rmtInit(38, RMT_TX_MODE, RMT_MEM_NUM_BLOCKS_1, 10000000);
   rgbLEDWrite(255, 0, 0);
 
-  BLEDevice::init("ESP32_MASTER");
+  BLEDevice::init("Tommy 的 A56");
+
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
@@ -92,7 +118,7 @@ void setup() {
 void loop() {
   if (newData) {
     newData = false;
-    Serial.print("dist:");  Serial.print(slaveData.ball_dist);
+    Serial.print("dist:");   Serial.print(slaveData.ball_dist);
     Serial.print(" angle:"); Serial.print(slaveData.ball_angle);
     Serial.print(" x:");     Serial.print(slaveData.robot_x);
     Serial.print(" y:");     Serial.println(slaveData.robot_y);
