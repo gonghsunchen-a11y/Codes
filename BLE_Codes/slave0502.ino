980cc70559f91806984f0325924d07855e39fd43
gonghsunchen-a11y <gonghsunchen@gmail.com>
2026-05-23 18:35:05 +0800

    Update slave0502.ino

--- BLE_Codes/slave0502.ino
+++ BLE_Codes/slave0502.ino
@@ -1,7 +1,9 @@
 #include <BLEDevice.h>
 
-#define SERVICE_UUID                 "458063a1-02bf-4664-857e-16c1030be066"
-#define DATA_CHARACTERISTIC_UUID     "a5209632-66a9-411d-9353-9be5507790fa"
+#define SERVICE_UUID                 "08001234-1234-4321-4321-123456789012"
+#define DATA_CHARACTERISTIC_UUID     "66666666-6666-6666-6666-666666666666"
+#define SECRET_KEY                   0x66
+#define AUTH_PASSWORD                0x66
 
 typedef struct {
   uint8_t ball_dist;
@@ -88,18 +90,26 @@ bool connectToServer(BLEAddress pAddress) {
 
   connected = true;
   rgbLEDWrite(0, 255, 0);
+
+  // 連上後先送密碼
+  uint32_t pwd = AUTH_PASSWORD;
+  pDataCharacteristic->writeValue((uint8_t*)&pwd, 4, false);
+  Serial.println("已送出密碼");
+  delay(200);
+
   return true;
 }
 
 void setup() {
   Serial.begin(115200);
-  TEENSY_SERIAL.begin(115200, SERIAL_8N1, 44, 43); // RX=44, TX=43
+  TEENSY_SERIAL.begin(115200, SERIAL_8N1, 44, 43);
   delay(2000);
 
   rmtInit(38, RMT_TX_MODE, RMT_MEM_NUM_BLOCKS_1, 10000000);
   rgbLEDWrite(255, 0, 0);
 
   BLEDevice::init("");
+
   BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
@@ -109,7 +119,6 @@ void setup() {
 }
 
 void loop() {
-  // BLE 連線
   if (doConnect) {
     if (connectToServer(*pServerAddress)) {
       Serial.println("Connected to Master");
@@ -126,16 +135,13 @@ void loop() {
     return;
   }
 
-  // 先發 0xAA 請求資料
   TEENSY_SERIAL.write(0xAA);
 
-  // 等 Teensy 回應
   unsigned long t = millis();
   while (TEENSY_SERIAL.available() < 6) {
-    if (millis() - t > 100) break; // 100ms timeout
+    if (millis() - t > 100) break;
   }
 
-  // 收 Teensy 資料
   if (TEENSY_SERIAL.available() >= 6) {
     if (TEENSY_SERIAL.read() == 0xBB) {
       myData.ball_dist  = TEENSY_SERIAL.read();
@@ -145,7 +151,12 @@ void loop() {
       uint8_t end       = TEENSY_SERIAL.read();
 
       if (end == 0xEE) {
-        pDataCharacteristic->writeValue((uint8_t*)&myData, sizeof(myData), false);
+        uint8_t buf[4];
+        buf[0] = myData.ball_dist  ^ SECRET_KEY;
+        buf[1] = myData.ball_angle ^ SECRET_KEY;
+        buf[2] = myData.robot_x   ^ SECRET_KEY;
+        buf[3] = myData.robot_y   ^ SECRET_KEY;
+        pDataCharacteristic->writeValue(buf, 4, false);
         Serial.print("Sent -> dist:"); Serial.print(myData.ball_dist);
         Serial.print(" angle:");       Serial.print(myData.ball_angle);
         Serial.print(" x:");           Serial.print(myData.robot_x);
